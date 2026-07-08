6627af3f10c7102ad4ec9d862e0d46df3d2faa7a
Kiswono Prayogo <kiswono@gmail.com>
2026-07-09 01:52:39 +0800

    Update vulnerable Vault dependencies

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module vault1
 
-go 1.20
+go 1.25.0
 
 require (
 	github.com/hashicorp/vault/api v1.9.2
@@ -9,7 +9,7 @@ require (
 
 require (
 	github.com/cenkalti/backoff/v3 v3.0.0 // indirect
-	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.5 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
@@ -22,8 +22,8 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/crypto v0.53.0 // indirect
+	golang.org/x/net v0.55.0 // indirect
+	golang.org/x/text v0.38.0 // indirect
 	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
 )
---
+++ scripts/verify-dependency-security.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+modules="$(go list -m -f '{{.Path}} {{.Version}}' all)"
+
+selected_version() {
+  awk -v module="$1" '$1 == module { print $2 }' <<<"$modules"
+}
+
+normalize_version() {
+  local version="${1#v}"
+  version="${version%%+*}"
+  printf '%s\n' "$version"
+}
+
+version_ge() {
+  local have want lowest
+  have="$(normalize_version "$1")"
+  want="$(normalize_version "$2")"
+  lowest="$(printf '%s\n%s\n' "$want" "$have" | sort -V | head -n 1)"
+  [[ "$lowest" == "$want" ]]
+}
+
+require_min() {
+  local module="$1"
+  local minimum="$2"
+  local version
+  version="$(selected_version "$module")"
+  if [[ -z "$version" ]]; then
+    return
+  fi
+  if ! version_ge "$version" "$minimum"; then
+    printf 'vulnerable module selected: %s %s < %s\n' "$module" "$version" "$minimum" >&2
+    exit 1
+  fi
+}
+
+require_min github.com/go-jose/go-jose/v3 v3.0.5
+require_min golang.org/x/crypto v0.53.0
+
+printf 'dependency security graph ok\n'
